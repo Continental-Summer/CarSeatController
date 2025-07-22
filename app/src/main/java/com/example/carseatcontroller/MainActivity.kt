a4788c46bb0bfdd43b8b8f5f027287f0b5dd5158
Stickey <36411522+Stickey21@users.noreply.github.com>
2025-07-22 11:36:26 +0300

    iconite hd

--- app/src/main/java/com/example/carseatcontroller/MainActivity.kt
+++ app/src/main/java/com/example/carseatcontroller/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.carseatcontroller
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -11,16 +13,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val seatImage = findViewById<ImageView>(R.id.seatImage)
+        seatImage.visibility = View.GONE
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_bluetooth -> {
+                    seatImage.setVisibility(View.GONE)
                     true
                 }
                 R.id.navigation_move -> {
+                    seatImage.setVisibility(View.VISIBLE)
                     true
                 }
                 R.id.navigation_massage -> {
+                    seatImage.setVisibility(View.VISIBLE)
                     true
                 }
                 else -> false
